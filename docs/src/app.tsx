df17b224953e9c9d5367a4c8ea7378aebdf7898c
TheKnarf <213777+TheKnarf@users.noreply.github.com>
2025-07-12 23:29:25 +0200

    Fix docs to use React Router DOM (#13)
    docs: update react-router usage

--- docs/src/app.tsx
+++ docs/src/app.tsx
@@ -1,4 +1,4 @@
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router-dom";
 
 const App = () => (
 	<div>
--- docs/src/ssg-main.tsx
+++ docs/src/ssg-main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
 const router = createBrowserRouter(routes);
